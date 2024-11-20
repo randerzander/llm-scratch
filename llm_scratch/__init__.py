2383f0a43b0b09a16ea7b21b4b6fd06b98703dfd
Randy Gelhausen <rgelhau@gmail.com>
2024-11-19 20:56:57 -0500

    bump

--- llm_scratch/__init__.py
+++ llm_scratch/__init__.py
@@ -1,4 +1,4 @@
-from .llm_scratch import mixtral, perplexity, llama3, haiku, opus, gpt4, llama_cpp, add_file, write_code
+from .llm_scratch import perplexity, haiku, opus, gpt4, llama_cpp, add_file, write_code
 
 from .raga import search, research, read_urls, find_relevant_content, get_plain_text_from_url, llm_answer
 
--- llm_scratch/llm_scratch.py
+++ llm_scratch/llm_scratch.py
@@ -9,36 +9,36 @@ def write_code(fn, llm_result):
     with open(fn, "w") as f:
         f.write(code)
 
-def perplexity(prompt: str, model="sonar-medium-online"):
-    url = "https://api.perplexity.ai/chat/completions"
-    perplexity_api_key = os.environ["PERPLEXITY_API_KEY"]
-    payload = {
-        "model": model,
-        "messages": [
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ],
-        'temperature': 1
-    }
-    headers = {
-        "Authorization": f"Bearer {perplexity_api_key}",
-        "accept": "application/json",
-        "content-type": "application/json"
-    }
+def perplexity(prompt: str, model="llama-3.1-sonar-large-128k-online"):
+    from openai import OpenAI
+    client = OpenAI(api_key=os.environ["PERPLEXITY_API_KEY"], base_url="https://api.perplexity.ai")
+    messages = [
+        {
+            "role": "system",
+            "content": (
+                "You are an artificial intelligence assistant and you need to "
+                "engage in a helpful, detailed, polite conversation with a user."
+            ),
+        },
+        {
+            "role": "user",
+            "content": (prompt)
+        },
+    ]
+    response = client.chat.completions.create(model=model, messages=messages)
+    return response.choices[0].message.content
 
-    response = requests.post(url, json=payload, headers=headers)
-    return response.json()['choices'][0]['message']['content']
-
-#def anthropic(prompt: str, model="claude-3-opus-20240229", max_tokens=2048):
 def haiku(prompt: str):
-    return anthropic(prompt, model="claude-3-haiku-20240307")
+    return anthropic(prompt, model="claude-3-5-haiku-latest")
 
 def opus(prompt: str):
-    return anthropic(prompt, model="claude-3-opus-20240229")
+    return anthropic(prompt, model="claude-3-opus-latest")
 
-def anthropic(prompt: str, model="claude-3-haiku-20240307", max_tokens=2048):
+def anthropic(prompt: str, model="claude-3-5-sonnet-latest", max_tokens=2048):
+    import anthropic
+    client = anthropic.Anthropic(
+        api_key=os.environ["ANTHROPIC_API_KEY"],
+    )
     message = client.messages.create(
         model=model,
         max_tokens=max_tokens,
@@ -46,7 +46,7 @@ def anthropic(prompt: str, model="claude-3-haiku-20240307", max_tokens=2048):
             {"role": "user", "content": prompt}
         ]
     )
-    return message.content
+    return message.content[0].text
 
 def make_messages(prompt):
     if type(prompt) == str:
@@ -90,12 +90,6 @@ def aiplayground(prompt, model="meta/llama3-70b", max_tokens=2000, temperature=0
       if chunk.choices[0].delta.content is not None:
           response += chunk.choices[0].delta.content
     return response
-
-def llama3(prompt, model="meta/llama3-70b", max_tokens=2000):
-    return aiplayground(prompt, model=model, max_tokens=max_tokens)
-
-def mixtral(prompt, max_tokens=2048):
-    return aiplayground(prompt, model="mistralai/mixtral-8x22b-instruct-v0.1", max_tokens=max_tokens)
 
 llm = None
 model_dir = "/home/dev/projects/models/"
